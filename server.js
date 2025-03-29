2fb94d82173d66334b73d627dcf77bb82d1f6cd7
abquintero <abquintero@uninorte.edu.co>
2025-03-29 15:58:50 -0500

    test

--- server.js
+++ server.js
@@ -1,43 +1,13 @@
+const path = require('path');
 const express = require('express');
 const dgram = require('dgram');
-const mysql = require('mysql2');
 const cors = require('cors');
-const path = require('path');
-const dotenv = require('dotenv');
-const fs = require("fs");
-const https = require("https");
-const http = require("http");
+const mysql = require('mysql2');
+// const dotenv = require('dotenv');
 
 const app = express();
-const port = process.env.PORT || 443; // Acceso sin :5000
+const port = process.env.PORT || 80; // Acceso sin :5000
 const udpPort = 5000; // Puerto UDP
-
-if (port === 443) {
-  //HTTPS server configuration
-  https.createServer({
-    key: fs.readFileSync(`/etc/letsencrypt/live/abquintero.ddns.net/privkey.pem`),
-    cert: fs.readFileSync(`/etc/letsencrypt/live/abquintero.ddns.net/fullchain.pem`)
-  }, app).listen(port, () => {
-
-    console.log(`HTTPS Server running on https://localhost:${port}`);
-  });
-  // HTTP to HTTPS redirection (listen on port 80)
-  http.createServer((req, res) => {
-    res.writeHead(301, { "Location": `https://abquintero.ddns.net` });
-    res.end();
-  }).listen(80, () => {
-    console.log(`HTTP server redirecting to HTTPS on port 80`);
-  });
-} else {
-  //HTTP server configuration
-  https.createServer({
-    key: fs.readFileSync(`/etc/letsencrypt/live/abquintero.ddns.net/privkey.pem`),
-    cert: fs.readFileSync(`/etc/letsencrypt/live/abquintero.ddns.net/fullchain.pem`)
-  }, app).listen(port, () => {
-
-    console.log(`HTTPS Testing Server running on https://localhost:${port}`);
-  });
-}
 
 // Conexión a MySQL
 require('dotenv').config();
@@ -127,5 +97,3 @@ udpServer.bind(udpPort, () => {
 app.listen(port, '0.0.0.0', () => {
   console.log("🚀 Servidor corriendo en http://0.0.0.0");
 });
-
-
