9af7dc32cbf48eea329070a9cece2e34b462db13
abquintero <abquintero@uninorte.edu.co>
2025-04-02 01:31:47 -0500

    muy bonito y todo pero el anterior commit fallaba :)

--- public/index.js
+++ public/index.js
@@ -17,123 +17,198 @@
   let liveRoute = null;
   let coordenadas = []; // Guarda el historial de coordenadas
   let liveCoords = [];
+  let liveRuns = 0;
+
   const messageEl = document.getElementById('message');
 
-  // Function to create or update a marker
-  function updateMarker(lat, lon, fecha, hora) {
-    const popupContent = `📍 Lat: ${lat}, Long: ${lon}<br>📅 ${fecha} ${hora}`;
+  // Inicializar el Autocomplete de Google Places
+  /* function initAutocomplete() {
 
-    // Check if lat and lon are valid numbers
-    if (isNaN(lat) || isNaN(lon)) {
-      console.error('Invalid coordinates:', lat, lon);
-      return; // Exit if coordinates are invalid
-    }
-
-    if (!marker) {
-      marker = L.marker([lat, lon]).addTo(map).bindPopup(popupContent);
-      map.openPopup(marker.getPopup()); // Open the popup explicitly
-    } else {
-      marker.setLatLng([lat, lon]).setPopupContent(popupContent);
-      if (marker.getPopup().isOpen()) {
-        // If the popup is already open, we can just update its content
-        marker.getPopup().setContent(popupContent);
-      } else {
-        map.openPopup(marker.getPopup()); // Open the popup explicitly
+    const input = document.getElementById('search-box');
+    console.log(input.value)
+    const autocomplete = new google.maps.places.PlaceAutocompleteElement(input);
+    autocomplete.addListener('place_changed', function () {
+      const place = autocomplete.getPlace();
+      if (!place.geometry) {
+        console.log("No se encontró la ubicación");
+        return;
       }
-    }
-  }
 
-  // Function to handle button activation
+      // Obtener coordenadas del lugar seleccionado
+      const lat = place.geometry.location.lat();
+      const lng = place.geometry.location.lng();
+
+      // Centrar el mapa en la nueva ubicación
+      map.setView([lat, lng], 14);
+
+      // Agregar o mover el marcador
+      if (marker) {
+        marker.setLatLng([lat, lng]);
+      } else {
+        marker = L.marker([lat, lng]).addTo(map);
+      }
+    });
+  } */
+
+  // Esperar a que la API de Google Maps cargue antes de inicializar
+  // window.onload = initAutocomplete;
+
+  // Función para resaltar el botón activo
   function resaltarBotonActivo(btn) {
-    document.querySelectorAll('button').forEach(b => b.classList.remove('active'));
+    // Quitar la clase active de todos los botones
+    const botones = document.querySelectorAll('button');
+    botones.map(b => b.classList.remove('active'));
+
+    // Agregar la clase active al botón clickeado
     btn.classList.add('active');
   }
 
-  async function fetchData(url) {
+  async function obtenerUltimaCoordenada() {
     try {
-      const response = await fetch(url);
+      const response = await fetch('/ultima-coordenada');
+
       const data = await response.json();
-      if (!data || data.error) throw new Error('Error fetching data');
+
+      if (!data || data.error) {
+        return error;
+      }
+
       return data;
-    } catch (error) {
-      console.error(error);
-      return null;
+    } catch (e) {
+      console.log(e);
+      return e;
     }
   }
 
-  async function obtenerUltimaCoordenada() {
-    return await fetchData('/ultima-coordenada');
-  }
-
+  // Función para obtener recorrido histórico
   async function obtenerRecorridoHistorico(inicio, fin) {
-    if (!inicio || !fin) throw new TypeError('"inicio" y "fin" son parámetros requeridos');
-    return await fetchData(`/recorrido-historico?inicio=${inicio}&fin=${fin}`);
+    if (!inicio || !fin) {
+      throw new TypeError('"inicio" y "fin" son parámetros requeridos');
+    }
+
+    try {
+      const response = await fetch(`/recorrido-historico?inicio=${inicio}&fin=${fin}`);
+
+      const data = await response.json();
+
+      if (!data.length) {
+        return;
+      }
+
+      return data;
+    } catch (e) {
+      console.error('❌ Error al obtener recorrido histórico:', error);
+    }
   }
 
   function substractArrayEvenly(arr, maxLength) {
     const n = arr.length;
+    const result = [];
+
     if (maxLength === 1) return [arr[n - 1]];
+
     const step = Math.floor((n - 1) / (maxLength - 1));
-    return [arr[0], ...Array.from({ length: maxLength - 2 }, (_, i) => arr[(i + 1) * step]), arr[n - 1]];
+
+    result.push(arr[0]);
+
+    for (let i = 1; i < maxLength - 1; i++) {
+      const index = i * step;
+      result.push(arr[index]);
+    }
+
+    result.push(arr[n - 1]);
+
+    return result;
   }
+
 
   async function solicitarRuta(puntos) {
     if (puntos.length < 2) return;
-    const coordenadasStr = substractArrayEvenly(puntos, 20).map(coord => `${coord[1]},${coord[0]}`).join(';');
-    const url = `https://router.project-osrm.org/route/v1/driving/${coordenadasStr}?overview=full&geometries=geojson`;
-    const data = await fetchData(url);
-    return data?.routes?.[0]?.geometry?.coordinates.map(coord => [coord[1], coord[0]]) || null;
+
+    let coordenadasStr = substractArrayEvenly(puntos, 20)
+      .map((coord) => `${coord[1]},${coord[0]}`)
+      .join(';');
+    let url = `https://router.project-osrm.org/route/v1/driving/${coordenadasStr}?overview=full&geometries=geojson`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (!data.routes || data.routes.length === 0) {
+        console.warn('⚠ No se encontró una ruta válida.');
+        return;
+      }
+
+      return data.routes[0].geometry.coordinates.map((coord) => [coord[1], coord[0]]);
+    } catch (error) {
+      console.error('❌ Error al solicitar la ruta:', error);
+    }
   }
 
-  function reiniciarRuta() {
-    console.log('🔄 Reiniciando recorrido...');
-    if (ruta) {
-      map.removeLayer(ruta); // Remove the route from the map if it exists
-      ruta = null; // Reset ruta to null after removal
-    }
-    if (liveRoute) {
-      map.removeLayer(liveRoute); // Remove the live route from the map if it exists
-      liveRoute = null; // Reset liveRoute to null after removal
-    }
-    coordenadas = []; // Reset history of coordinates
-    liveCoords = []; // Reset live coordinates
-  }
+  const mapContainer = document.getElementById('map');
+  const tiempoRealBtn = document.getElementById('tiempo-real-btn');
+  const historicoBtn = document.getElementById('historico-btn');
+  const reiniciarBtn = document.getElementById('reiniciar-btn');
+  const switchHistoricoBtn = document.getElementById('switch-historico-btn');
+  const inicioInput = document.getElementById('inicio');
+  const finInput = document.getElementById('fin');
+  const historicoControlsInput = document.getElementById('historico-controls');
+
+  // CON ESTO SE RESALTAN LOS BOTONES
+  tiempoRealBtn.addEventListener('click', () => {
+    resaltarBotonActivo(tiempoRealBtn); // Resalta el botón de Tiempo Real
+  });
+
+  switchHistoricoBtn.addEventListener('click', () => {
+    resaltarBotonActivo(switchHistoricoBtn); // Resalta el botón de Historial
+  });
+
 
   function formatearFecha(fromServer, fecha, hora) {
     return fromServer ? fecha.replace('T00:00:00.000Z', ' ' + hora) : fecha.replace('T', ' ').replace('Z', '');
   }
 
-  // Event listeners
-  const tiempoRealBtn = document.getElementById('tiempo-real-btn')
-  const historicoBtn = document.getElementById('historico-btn')
+  function reiniciarRuta() {
+    console.log('🔄 Reiniciando recorrido...');
+    if (ruta) {
+      map.removeLayer(ruta); // Eliminar la ruta del mapa
+    }
 
-  tiempoRealBtn.addEventListener('click', () => resaltarBotonActivo(tiempoRealBtn));
-  historicoBtn.addEventListener('click', () => resaltarBotonActivo(historicoBtn));
+    if (liveRoute) {
+      map.removeLayer(liveRoute);
+    }
+    coordenadas = []; // Reiniciar historial de coordenadas
+    liveCoords = [];
+  }
 
-  document.getElementById('reiniciar-btn').addEventListener('click', reiniciarRuta);
-  document.getElementById('switch-historico-btn').addEventListener('click', () => {
-    const historicoControlsInput = document.getElementById('historico-controls');
-    historicoControlsInput.classList.toggle('hidden');
+  reiniciarBtn.addEventListener('click', reiniciarRuta);
+
+  switchHistoricoBtn.addEventListener('click', async () => {
+    return historicoControlsInput.classList.contains('hidden')
+      ? historicoControlsInput.classList.remove('hidden')
+      : historicoControlsInput.classList.add('hidden');
   });
 
-  async function handleHistorico() {
+  historicoBtn.addEventListener('click', async () => {
     reiniciarRuta();
     const ultimaCoord = await obtenerUltimaCoordenada();
-    const inicio = formatearFecha(false, document.getElementById('inicio').value);
-    const fin = formatearFecha(false, document.getElementById('fin').value);
-
-    if (!inicio || !fin) {
+    if (!inicioInput.value || !finInput.value) {
       messageEl.classList.remove('hidden');
       messageEl.classList.add('error');
       messageEl.textContent = 'Debe llenar los campos de inicio y fin';
       return;
     }
 
+    // ✅ Aquí ocultamos el mensaje si los valores son correctos
     messageEl.classList.add('hidden');
     messageEl.classList.remove('error');
     messageEl.textContent = '';
 
-    const historico = await obtenerRecorridoHistorico(inicio, fin);
+    const historico = await obtenerRecorridoHistorico(
+      formatearFecha(false, inicioInput.value),
+      formatearFecha(false, finInput.value)
+    );
+
     if (!historico || historico.length === 0) {
       messageEl.classList.remove('hidden');
       messageEl.classList.add('error');
@@ -141,61 +216,111 @@
       return;
     }
 
-    const rutaCoords = historico.map(coord => [parseFloat(coord.latitud), parseFloat(coord.longitud)]);
+    const rutaCoords = historico.map((coord) => [parseFloat(coord.latitud), parseFloat(coord.longitud)]);
     const rutaPlacement = await solicitarRuta(rutaCoords);
-    if (ruta) map.removeLayer(ruta);
+
+    if (ruta) map.removeLayer(ruta); // Eliminar ruta anterior
 
     const [lat, lon] = [ultimaCoord.latitud, ultimaCoord.longitud];
-    updateMarker(lat, lon, ultimaCoord.fecha, ultimaCoord.hora);
+
+    if (!marker) {
+      marker = L.marker([lat, lon])
+        .addTo(map)
+        .bindPopup(`📍 Lat: ${lat}, Long: ${lon}<br>📅 ${ultimaCoord.fecha} ${ultimaCoord.hora}`)
+        .openPopup();
+    } else {
+      marker
+        .setLatLng([lat, lon])
+        .setPopupContent(`📍 Lat: ${lat}, Long: ${lon}<br>📅 ${ultimaCoord.fecha} ${ultimaCoord.hora}`)
+        .openPopup();
+    }
+
     ruta = new L.polyline(rutaPlacement, { color: 'red', weight: 4 }).addTo(map);
-  }
 
-  document.getElementById('historico-btn').addEventListener('click', handleHistorico);
+    // map.fitBounds(ruta.getBounds());
+  });
 
-  async function iniciarTiempoReal() {
+  async function iniciarTiempoReal(intervalId = null, from = '') {
+    console.log(from);
+    historicoControlsInput.classList.add('hidden');
     reiniciarRuta();
+
+    if (intervalId) clearInterval(intervalId);
     const ultimaCoord = await obtenerUltimaCoordenada();
-    const rutaCoords = [[ultimaCoord.latitud, ultimaCoord.longitud], [ultimaCoord.latitud, ultimaCoord.longitud]];
+
+    const currentDate = new Date(formatearFecha(true, ultimaCoord.fecha, ultimaCoord.hora));
+    const substractHours = (d, n) =>
+      new Date(d.setHours(d.getHours() - n)).toISOString().replace('T', ' ').substring(0, 19);
+
+    const rutaCoords = [[ultimaCoord.latitud, ultimaCoord.longitud], [ultimaCoord.latitud, ultimaCoord.longitud]]// historico.map((coord) => [parseFloat(coord.latitud), parseFloat(coord.longitud)]);
     coordenadas = rutaCoords;
+    coordenadas.push([ultimaCoord.latitud, ultimaCoord.longitud]);
 
     const rutaPlacement = await solicitarRuta(coordenadas);
-    if (ruta) map.removeLayer(ruta);
-    updateMarker(ultimaCoord.latitud, ultimaCoord.longitud, ultimaCoord.fecha, ultimaCoord.hora);
-    ruta = new L.polyline(rutaPlacement, { color: 'red', weight: 4 }).addTo(map);
 
-    setInterval(actualizarMapa, 5000);
+    if (ruta) map.removeLayer(ruta); // Eliminar ruta anterior
+
+    const [lat, lon] = [ultimaCoord.latitud, ultimaCoord.longitud];
+
+    if (!marker) {
+      marker = L.marker([lat, lon])
+        .addTo(map)
+        .bindPopup(`📍 Lat: ${lat}, Long: ${lon}<br>📅 ${ultimaCoord.fecha} ${ultimaCoord.hora}`)
+        .openPopup();
+    } else {
+      marker
+        .setLatLng([lat, lon])
+        .setPopupContent(`📍 Lat: ${lat}, Long: ${lon}<br>📅 ${ultimaCoord.fecha} ${ultimaCoord.hora}`)
+        .openPopup();
+    }
+
+    ruta = new L.polyline(rutaPlacement, { color: 'red', weight: 4 }).addTo(map);
+    
+    /*if (liveRuns === 0) {
+      map.fitBounds(ruta.getBounds());
+      map.setView([lat, lon], 15);
+    }
+    console.log(liveRuns)
+    liveRuns =+ 1;*/
+
+    intervalId = setInterval(actualizarMapa, 5000);
   }
 
-  document.getElementById('tiempo-real-btn').addEventListener('click', iniciarTiempoReal);
+  tiempoRealBtn.addEventListener('click', async () => {
+    await iniciarTiempoReal(null, 'RUNNING FROM CLICK')
+  });
+
+  await iniciarTiempoReal(null, 'RUNNING FROM INIT')
 
   async function actualizarMapa() {
     const ultimaCoord = await obtenerUltimaCoordenada();
-    const [lat, lon] = [ultimaCoord.latitud, ultimaCoord.longitud];
     liveCoords.push([ultimaCoord.latitud, ultimaCoord.longitud]);
+
     const rutaPlacement = await solicitarRuta(liveCoords.length <= 1 ? [liveCoords[0], liveCoords[0]] : liveCoords);
-    if (liveRoute) map.removeLayer(liveRoute);
-    updateMarker(ultimaCoord.latitud, ultimaCoord.longitud, ultimaCoord.fecha, ultimaCoord.hora);
+
+    if (liveRoute) map.removeLayer(liveRoute); // Eliminar ruta anterior
+
+    const [lat, lon] = [ultimaCoord.latitud, ultimaCoord.longitud];
+
+    if (!marker) {
+      marker = L.marker([lat, lon])
+        .addTo(map)
+        .bindPopup(`📍 Lat: ${lat}, Long: ${lon}<br>📅 ${ultimaCoord.fecha} ${ultimaCoord.hora}`)
+        .openPopup();
+    } else {
+      marker
+        .setLatLng([lat, lon])
+        .setPopupContent(`📍 Lat: ${lat}, Long: ${lon}<br>📅 ${ultimaCoord.fecha} ${ultimaCoord.hora}`)
+        .openPopup();
+    }
+
     liveRoute = new L.polyline(rutaPlacement, { color: 'blue', weight: 4 }).addTo(map);
-    map.fitBounds(liveRoute.getBounds());
-    if (map) map.setView([lat, lon], 20);
+
+    //const currentZoom = map.getZoom();
+    map.setView([lat, lon], 20);
   }
 
-  // await actualizarMapa()
-
-  function obtenerFechaHoraActual() {
-    const ahora = new Date();
-    const año = ahora.getFullYear();
-    const mes = String(ahora.getMonth() + 1).padStart(2, '0');
-    const dia = String(ahora.getDate()).padStart(2, '0');
-    const fechaHoy = `${año}-${mes}-${dia}T00:00`;
-    const hora = String(ahora.getHours()).padStart(2, '0');
-    const minutos = String(ahora.getMinutes()).padStart(2, '0');
-    const finDefecto = `${año}-${mes}-${dia}T${hora}:${minutos}`;
-    document.getElementById('inicio').value = fechaHoy;
-    document.getElementById('fin').value = finDefecto;
-  }
-
-  obtenerFechaHoraActual();
+  obtenerFechaHoraActual()
 
   fetch('/config')
     .then(response => response.json())
@@ -203,5 +328,33 @@
       document.getElementById('titulo').textContent = `Mapa MyCoords - ${data.nombre}`;
     })
     .catch(error => console.error('Error al obtener el nombre:', error));
+
+  // FUNCIÓN PARA RECIBIR CON ALGO EN EL CALENDARIO
+  function obtenerFechaHoraActual() {
+    const ahora = new Date();
+
+    // Obtener la fecha en formato YYYY-MM-DD
+    const año = ahora.getFullYear();
+    const mes = String(ahora.getMonth() + 1).padStart(2, '0');
+    const dia = String(ahora.getDate()).padStart(2, '0');
+
+    // Formato para el campo datetime-local
+    const fechaHoy = `${año}-${mes}-${dia}`;
+    const inicioDefecto = `${fechaHoy}T00:00`;
+
+    // Obtener la hora actual en formato HH:MM
+    const hora = String(ahora.getHours()).padStart(2, '0');
+    const minutos = String(ahora.getMinutes()).padStart(2, '0');
+    const finDefecto = `${fechaHoy}T${hora}:${minutos}`;
+
+    // Asignar valores a los inputs
+    document.getElementById('inicio').value = inicioDefecto;
+    document.getElementById('fin').value = finDefecto;
+  }
+
+  function toggleHistorico() {
+    const historicoContainer = document.getElementById('historicoContainer');
+    historicoContainer.style.display = historicoContainer.style.display === 'none' ? 'block' : 'none';
+  }
 
 })();
