c1a2f79a8d2afb19f07691ef07d5bbc1b1db3ba3
abquintero <abquintero@uninorte.edu.co>
2025-04-08 21:49:30 -0500

    cambio margen recorrido historico 5

--- public/index.js
+++ public/index.js
@@ -410,7 +410,7 @@
   async function solicitarRuta(puntos) {
     if (puntos.length < 2) return;
 
-    let coordenadasStr = substractArrayEvenly(puntos, 100)
+    let coordenadasStr = substractArrayEvenly(puntos, 150)
       .map((coord) => `${coord[1]},${coord[0]}`)
       .join(';');
     let url = `https://router.project-osrm.org/route/v1/driving/${coordenadasStr}?overview=full&geometries=geojson`;
