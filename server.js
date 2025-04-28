9f3b860cb778972398f04c9d1e181d706c4c9f29
abquintero <abquintero@uninorte.edu.co>
2025-04-28 13:26:09 -0500

    base de datos 9

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const db = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0
 });
- 
+
 console.log('✅ Conexión exitosa a la base de datos.');
 
 // Add the periodic ping right here
@@ -80,33 +80,33 @@ const udpServer = dgram.createSocket('udp4');
 udpServer.on('message', (msg, rinfo) => {
   console.log(`📩 Mensaje recibido de ${rinfo.address}:${rinfo.port} -> ${msg}`);
 
-  
+
   // Reemplazar saltos de línea por espacios
   const cleanMsg = msg.toString().replace(/\n/g, ' ');
- 
+
   const data = cleanMsg.toString().match(/Latitud:\s*([-0-9.]+)\s*Longitud:\s*([-0-9.]+)\s*Fecha y Hora GPS:\s*([\d-]+\s[\d:]+)\s*RPM:\s*(\d+)/);
   if (data) {
     const latitud = parseFloat(data[1]);
     const longitud = parseFloat(data[2]);
     const [fecha, hora] = data[3].split(' ');
     const rpm = parseInt(data[4]);
- 
+
     db.query('INSERT INTO coordenadas (latitud, longitud, fecha, hora, rpm) VALUES (?, ?, ?, ?, ?)',
       [latitud, longitud, fecha, hora, rpm], (err) => {
         if (err) console.error('❌ Error al insertar en MySQL:', err.message);
         else console.log(`📌 Coordenada guardada: Lat: ${latitud}, Long: ${longitud}, RPM: ${rpm}`);
       });
-   }
- });
+  }
+});
 
 // Endpoint para buscar coordenadas dentro del círculo
 app.get('/buscar-por-area', (req, res) => {
   const { lat, lng, radio, inicio, fin } = req.query;
-  
+
   if (!lat || !lng || !radio || !inicio || !fin) {
     return res.status(400).json({ error: 'Faltan parámetros necesarios' });
   }
-  
+
   // Consulta que utiliza la fórmula de Haversine para calcular distancias
   const query = `
     SELECT *, 
@@ -115,7 +115,7 @@ app.get('/buscar-por-area', (req, res) => {
     WHERE CONCAT(fecha, " ", hora) BETWEEN ? AND ? 
     HAVING distancia <= ? 
     ORDER BY fecha ASC, hora ASC`;
-  
+
   db.query(
     query,
     [lat, lng, lat, inicio, fin, radio/1000], // Convertimos metros a kilómetros
@@ -133,7 +133,7 @@ app.get('/buscar-por-area', (req, res) => {
 app.get('/config', (req, res) => {
   res.json({ nombre: process.env.NOMBRE });
 });
- 
+
 udpServer.bind(udpPort, () => {
   console.log(`✅ Servidor UDP escuchando en el puerto ${udpPort}`);
 });
